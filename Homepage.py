f633a81755f009d6ab47ce2ab0f84682a485d30e
Prashant Panchal <64975859+prashant263-del@users.noreply.github.com>
2024-03-15 18:36:40 +0530

    Update Homepage.py folder added

--- Homepage.py
+++ Homepage.py
@@ -1,19 +1,5 @@
 import streamlit as st
-
-st.set_page_config(
-    page_title="TA Chatbot",
-    page_icon="🤖",)
-
-st.title("Main Page")
-
-# Add "Talent Analytics Chatbot" as content
-st.header("Talent Analytics Chatbot")
-st.write("Welcome to the Talent Analytics Chatbot. Here, you can interact with our chatbot to get insights on your data.")
-
-# Add a sidebar with a selectbox
-# selected_page = st.sidebar.selectbox("Select a page", ["Page 1", "Page 2", "Page 3"])
-
-# Show a message based on the selected pa
+import os
 
 # Get the path to the folder containing the files
 folder_path = "/Pages"
