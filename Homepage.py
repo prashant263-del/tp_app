c2921e7a6b325dd7d94c1ab76e54e57bd2095518
Prashant Panchal <64975859+prashant263-del@users.noreply.github.com>
2024-03-15 18:45:24 +0530

    Update Homepage.py asadacsc

--- Homepage.py
+++ Homepage.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 
 # Set the page configuration
@@ -7,11 +8,9 @@ st.set_page_config(
 )
 
 # Define the paths to your pages
-page_paths = {
-    "Page 1": "./Pages/Chatbot.py",
-    "Page 2": "/path/to/your/page2.py",
-    "Page 3": "/path/to/your/page3.py",
-}
+page_dir = "Pages"
+page_files = os.listdir(page_dir)
+page_paths = {file[:-3]: os.path.join(page_dir, file) for file in page_files if file.endswith(".py")}
 
 # Display the title and header
 st.title("Main Page")
