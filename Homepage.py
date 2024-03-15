82264161dfd3fa1f69a9f2fa5845509d5eb3a070
Prashant Panchal <64975859+prashant263-del@users.noreply.github.com>
2024-03-15 18:18:34 +0530

    test

---
+++ Homepage.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="TA Chatbot",
+    page_icon="🤖",)
+
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
+
+# Add "Talent Analytics Chatbot" as content
+st.header("Talent Analytics Chatbot")
+st.write("Welcome to the Talent Analytics Chatbot. Here, you can interact with our chatbot to get insights on your data.")
+
+# Add more content if needed
+
+
